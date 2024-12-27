96f1595206fcb160dbd45d5be2f51154f8b4be4f
Leonardo <leonardfmr@gmail.com>
2024-12-27 08:45:18 -0600

    Changes in the color

--- src/components/Botones/Botones.js
+++ src/components/Botones/Botones.js
@@ -3,8 +3,8 @@ import styles from "./Botones.module.css"
 function Botones({ onCrear, onLimpiar }) {
     return (
         <div className={styles.contenedorBotones}>
-            <button className={styles.botonCrear} onClick={onCrear}>Crear</button>
-            <button className={styles.botonLimpiar} onClick={onLimpiar}>Limpiar</button>
+            <button className={styles.botonCrear} onClick={onCrear}>GUARDAR</button>
+            <button className={styles.botonLimpiar} onClick={onLimpiar}>LIMPIAR</button>
         </div>
     );
 }
